71c5981bde765da2bdce52b0ff1581cfc98d7c8a
Karl Duggan <52557899+karlduggan@users.noreply.github.com>
2023-04-18 14:25:42 +0100

    Create blockchain_v2.py

---
+++ simple_blockchain/blockchain_v2.py
@@ -0,0 +1,66 @@
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self, data, previous_hash):
+        self.timestamp = datetime.datetime.now()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block = str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)
+        hash = hashlib.sha256(block.encode()).hexdigest()
+        return hash
+
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 2
+
+    def create_genesis_block(self):
+        return Block("Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        previous_block = self.get_latest_block()
+        new_block = Block(data, previous_block.hash)
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+        return True
+
+    def print_chain(self):
+        for block in self.chain:
+            print(f"Timestamp: {block.timestamp}")
+            print(f"Data: {block.data}")
+            print(f"Previous Hash: {block.previous_hash}")
+            print(f"Hash: {block.hash}")
+            print("\n")
+
+# Create a new blockchain
+blockchain = Blockchain()
+
+# Add new blocks to the blockchain
+blockchain.add_block("Block 1")
+blockchain.add_block("Block 2")
+blockchain.add_block("Block 3")
+
+# Print the entire blockchain
+blockchain.print_chain()
